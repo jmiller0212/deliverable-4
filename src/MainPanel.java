b2a6c643f42bfaf1293df772668ae220cad57d3f
Jarod Miller <jcmiller0212@gmail.com>
2021-04-05 17:24:18 -0400

    second

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -141,7 +141,7 @@ public class MainPanel extends JPanel {
 		_backupCells = new Cell[_size][_size];
 		for (int j = 0; j < _size; j++) {
 			for (int k = 0; k < _size; k++) {
-				_backupCells[j][k] = new Cell();
+//				_backupCells[j][k] = new Cell();
 				_backupCells[j][k].setAlive(_cells[j][k].getAlive());
 			}
 		}
